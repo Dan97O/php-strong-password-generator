ef051a0bc5b4b9c5d8bafe18f434ea8b48181d6a
Dan97O <dan97-oddo@live.it>
2023-05-05 14:25:04 +0200

    add functions.php and require at index.php

---
+++ functions.php
@@ -0,0 +1,14 @@
+
+<?php
+function passwordGenerate($length)
+{
+    $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!"#$%&/\():;<>*+-.,=?@[]^_`{|}~';
+    $password = '';
+    for ($i = 0; $i < $length; $i++) {
+        $password .= $chars[random_int(0, strlen($chars) - 1)];
+    }
+    return $password;
+}
+//$length = 10;
+//var_dump(passwordGenerate($length))
+?>
